9eceb95201790ae2c3b757f0bc96a882896e08bb
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-05 00:12:22 +0600

    rechart are added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import useData from '../../Hooks/useData';
 import './Dashboard.css';
 
 const Dashboard = () => {
+    const [d, setData] = useData();
+    console.log(d);
     return (
-        <div>
-            <h1>Its Dashboard</h1>
+        <div className='w-full h-96 mt-40'>
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                    data={d}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="investment"/>
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     );
 };
