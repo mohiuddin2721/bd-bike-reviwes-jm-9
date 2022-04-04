254f401c20876f22369486ad768aafd8c0ebb628
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-05 00:26:44 +0600

    rechart complite and it's working perfectly

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import useData from '../../Hooks/useData';
 import './Dashboard.css';
 
@@ -7,7 +7,7 @@ const Dashboard = () => {
     const [d, setData] = useData();
     console.log(d);
     return (
-        <div className='w-full h-96 mt-40'>
+        <div className='w-full h-96 mt-20'>
             <ResponsiveContainer width="100%" height="100%">
                 <LineChart
                     data={d}
@@ -20,12 +20,32 @@ const Dashboard = () => {
                 >
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="month" />
-                    <YAxis dataKey="investment"/>
+                    <YAxis dataKey="investment" />
                     <Tooltip />
                     <Legend />
                     <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
                     <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
                 </LineChart>
+            </ResponsiveContainer>
+
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                    data={d}
+                    margin={{
+                        top: 20,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="investment" />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="sell" stackId="a" fill="#8884d8" />
+                    <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+                </BarChart>
             </ResponsiveContainer>
         </div>
     );
