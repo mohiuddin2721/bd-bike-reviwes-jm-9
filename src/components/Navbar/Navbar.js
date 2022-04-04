7048299f27a3c50e4d12656bb3521b9d3ac7e3f0
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-04 13:41:33 +0600

    navbar added and it's working perfectly

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import { faBars, faCross } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
 
+
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const routes = [
         {id: 1, name: 'HOME', link: '/home'},
         {id: 1, name: 'REVIEWS', link: '/reviews'},
@@ -10,8 +14,11 @@ const Navbar = () => {
         {id: 1, name: 'ABOUT', link: '/about'}
     ]
     return (
-        <nav>
-            <ul className='md:flex justify-center'>
+        <nav className='bg-indigo-100'>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open ? <FontAwesomeIcon icon={faCross}></FontAwesomeIcon> : <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>}
+            </div>
+            <ul  className={`md:flex md:text-lg md:font-bold md:py-6 justify-center absolute md:static bg-indigo-100 w-full duration-500 ease-in ${open ? 'top-6': 'top-[-120px]'}`}>
                 {
                     routes.map(route => <Link 
                         key={route.id} 
